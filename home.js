6bbb78b5e89b3705d787fa6ddc572e3ad2391406
JPA-EliteDeveloper <algadipej962@gmail.com>
2023-03-20 20:28:39 +0800

    save

---
+++ home.js
@@ -0,0 +1,74 @@
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 5000); // Change image every 5 seconds
+}
+
+
+const trigger = document.querySelector("menu > .trigger");
+trigger.addEventListener('click', (e) => {
+    e.currentTarget.parentElement.classList.toggle("open");
+});
+
+
+
+// Social Links Button Function
+function myFunction0() {
+  var x = document.getElementById("Demo0");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+function myFunction1() {
+  var x = document.getElementById("Demo1");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+function myFunction2() {
+  var x = document.getElementById("Demo2");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+function myFunction3() {
+  var x = document.getElementById("Demo3");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+
+// Show Images Animation
+
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.getElementById("img-map").className = "slideUp";
+    document.getElementById("img-map").className = "slideUp"; }
+
+}
+
+
